ec6b391bd1ed540ef6a6f9a5316c9b13a5814b43
Alex Saju <alexsajued@gmail.com>
2025-07-02 16:16:17 +0530

    Update pointersort.c

--- pointersort.c
+++ pointersort.c
@@ -1,6 +1,55 @@
-#include<stdio.h>
-void main()
-{
-int n,i=0;
-printf("enter no of elements");
+#include <stdio.h>
 
+int main() {
+    int arr[100], n, i, j, temp;
+    FILE *fp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", (arr + i)); 
+    }
+
+   
+    fp = fopen("array.txt", "w");
+    if(fp == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+    fprintf(fp, "Unsorted array:\n");
+    for(i = 0; i < n; i++) {
+        fprintf(fp, "%d ", *(arr + i)); 
+    }
+    fprintf(fp, "\n");
+
+    
+    for(i = 0; i < n-1; i++) {
+        for(j = 0; j < n-i-1; j++) {
+            if(*(arr + j) > *(arr + j + 1)) {
+                temp = *(arr + j);
+                *(arr + j) = *(arr + j + 1);
+                *(arr + j + 1) = temp;
+            }
+        }
+    }
+
+
+    fprintf(fp, "Sorted array:\n");
+    for(i = 0; i < n; i++) {
+        fprintf(fp, "%d ", *(arr + i));
+    }
+    fprintf(fp, "\n");
+    fclose(fp);
+
+    
+    printf("Sorted array:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", *(arr + i));
+    }
+    printf("\n");
+
+    printf("Arrays written to file 'array.txt'.\n");
+    return 0;
+}
