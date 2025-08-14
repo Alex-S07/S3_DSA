fb6670f8b3e0f3a1b770e75f107d5cd3f2ce7699
Alex Saju <alexsajued@gmail.com>
2025-08-14 13:51:45 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,52 +1,70 @@
-#include <stdio.h>
+#include<stdio.h>
 
-struct polyn {
+struct poly {
     int coeff;
-    int expo;
-} poly1[100], poly2[100], poly3[100]; 
+    int exp;
+} poly1[10], poly2[10], poly3[20];
 
 int main() {
-    int n1, n2, i, k = 0;
-
-    printf("Enter the number of terms in the first polynomial: ");
+    int n1, n2, i, j, k;
+    
+    printf("enter the no.of terms of 1st poly:");
     scanf("%d", &n1);
     for (i = 0; i < n1; i++) {
-        printf("Enter the coefficient for term %d: ", i + 1);
+        printf("enter the coefficent:");
         scanf("%d", &poly1[i].coeff);
-        printf("Enter the exponent for term %d: ", i + 1);
-        scanf("%d", &poly1[i].expo);
+        printf("enter the exponent:");
+        scanf("%d", &poly1[i].exp);
     }
-
-   
-    printf("Enter the number of terms in the second polynomial: ");
+    
+    printf("enter the no.of terms of 2nd poly:");
     scanf("%d", &n2);
     for (i = 0; i < n2; i++) {
-        printf("Enter the coefficient for term %d: ", i + 1);
+        printf("enter the coefficent:");
         scanf("%d", &poly2[i].coeff);
-        printf("Enter the exponent for term %d: ", i + 1);
-        scanf("%d", &poly2[i].expo);
+        printf("enter the exponent:");
+        scanf("%d", &poly2[i].exp);
     }
-
-
-
-    for (i = 0; i < n1; i++) {
-        if (poly1[i].expo != poly2[i].expo) {
-            printf("Error: The exponents of the polynomials do not match at term %d.\n", i + 1);
-            return 1;  
-        }
-     
-        poly3[i].coeff = poly1[i].coeff + poly2[i].coeff;
-        poly3[i].expo = poly1[i].expo;  
-    }
-
     
-    printf("Resultant polynomial after addition:\n");
-    for (i = 0; i < n1; i++) {
-        printf("%dx^%d", poly3[i].coeff, poly3[i].expo);
-        if (i < n1 - 1)
-            printf(" + ");
+    i = j = k = 0;
+    
+    while (i < n1 && j < n2) {
+        if (poly1[i].exp == poly2[j].exp) {
+            poly3[k].coeff = poly1[i].coeff + poly2[j].coeff;
+            poly3[k].exp = poly1[i].exp;
+            i++;
+            j++;
+            k++;
+        } else if (poly1[i].exp > poly2[j].exp) {
+            poly3[k].coeff = poly1[i].coeff;
+            poly3[k].exp = poly1[i].exp;
+            i++;
+            k++;
+        } else {
+            poly3[k].coeff = poly2[j].coeff;
+            poly3[k].exp = poly2[j].exp;
+            j++;
+            k++;
+        }
     }
-    printf("\n");
-
+    
+    while (i < n1) {
+        poly3[k].coeff = poly1[i].coeff;
+        poly3[k].exp = poly1[i].exp;
+        i++;
+        k++;
+    }
+    
+    while (j < n2) {
+        poly3[k].coeff = poly2[j].coeff;
+        poly3[k].exp = poly2[j].exp;
+        j++;
+        k++;
+    }
+    
+   for(i=0;i<k-1;i++)
+ 			printf("%d(x)^%d + ",poly3[i].coeff,poly3[i].exp);
+ 	printf("%d(x)^%d",poly3[i].coeff,poly3[i].exp);
+    
     return 0;
 }
